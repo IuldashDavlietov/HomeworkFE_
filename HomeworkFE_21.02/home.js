e2ff7660e250608545eecc2a35ffb06d1d1d3a88
IuldashDavlietov <118936839+IuldashDavlietov@users.noreply.github.com>
2023-02-25 20:31:16 +0100

    Add files via upload

---
+++ HomeworkFE_21.02/home.js
@@ -0,0 +1,28 @@
+//let number = prompt('Select a number' + '')
+//alert(number ** 2)
+
+// let num = prompt('Select a number')
+
+// if(+num === 0){
+// console.log('chislo ravno nuliu')
+// } 
+// if(+num % 2===0 && +num!==0) {
+//     console.log('chislo chetnoe')
+// }
+// if(+num % 2 === 1){
+//     console.log('chislo nechetnoe')
+// }
+
+let variable = prompt('Enter the value')
+    
+    if( variable == +variable){
+    console.log('Your value can be transformed into a number')
+    } else{
+        console.log('It will be impossible to convert your value to a number')
+    }
+
+
+
+    
+
+
